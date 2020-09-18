2190a5616e0541a2a2b4e2a21491763d571e6030
Sanyam Jain <jain.sanyam786@gmail.com>
2020-09-17 22:02:33 -0400

    Added code for DFS and iterative maze generation

--- Project1/Maze.py
+++ Project1/Maze.py
@@ -4,15 +4,12 @@ import matplotlib.cm as cm
 import datetime as t
 
 
-def create_mazes(startsize, maxsize, p):
-    mazes = []
-    for index, size in enumerate(range(startsize, maxsize, 10)):
-        num = [[0 if np.random.random() > p else 1 for i in range(size)] for j in range(size)]
-        num_arr = np.array(num)
-        num_arr[0][0] = 0
-        num_arr[size - 1][size - 1] = 0
-        mazes.append(num_arr)
-    return mazes
+def create_maze(size, p):
+    num = [[0 if np.random.random() > p else 1 for i in range(size)] for j in range(size)]
+    num_arr = np.array(num)
+    num_arr[0][0] = 0
+    num_arr[size - 1][size - 1] = 0
+    return num_arr
 
 
 def create_graph(maze):
@@ -26,35 +23,35 @@ def create_graph(maze):
                 # corner
                 if check_corner(i, j, maze):
                     if (i, j) == (0, 0):
-                        edges = get_neighbour(maze, [(1, 0), (0, 1)], i, j)
+                        edges = get_neighbour(maze, [(0, 1), (1, 0)], i, j)
                     elif (i, j) == (maze.shape[0] - 1, maze.shape[1] - 1):
                         edges = get_neighbour(maze, [(-1, 0), (0, -1)], i, j)
                     elif (i, j) == (0, maze.shape[1] - 1):
-                        edges = get_neighbour(maze, [(1, 0), (0, -1)], i, j)
+                        edges = get_neighbour(maze, [(0, -1), (1, 0)], i, j)
                     elif (i, j) == (maze.shape[0] - 1, 0):
                         edges = get_neighbour(maze, [(-1, 0), (0, 1)], i, j)
                 # Middle part
                 elif check_middle(i, j, maze):
-                    edges = get_neighbour(maze, [(-1, 0), (0, -1), (1, 0), (0, 1)], i, j)
+                    edges = get_neighbour(maze, [(-1, 0), (0, -1), (0, 1), (1, 0)], i, j)
                 # top layer excluding corners
                 elif check_top(i, j, maze):
-                    edges = get_neighbour(maze, [(0, -1), (1, 0), (0, 1)], i, j)
+                    edges = get_neighbour(maze, [(0, -1), (0, 1), (1, 0)], i, j)
                 # left layer excluding corners
                 elif check_left(i, j, maze):
-                    edges = get_neighbour(maze, [(0, 1), (1, 0), (-1, 0)], i, j)
+                    edges = get_neighbour(maze, [(-1, 0), (0, 1), (1, 0)], i, j)
                 # bottom layer excluding corners
                 elif check_bottom(i, j, maze):
-                    edges = get_neighbour(maze, [(0, -1), (0, 1), (-1, 0)], i, j)
+                    edges = get_neighbour(maze, [(-1, 0), (0, -1), (0, 1)], i, j)
 
                 # right layer excluding corners
                 elif check_right(i, j, maze):
-                    edges = get_neighbour(maze, [(-1, 0), (1, 0), (0, -1)], i, j)
+                    edges = get_neighbour(maze, [(-1, 0), (0, -1), (1, 0)], i, j)
                 graph[(i, j)] = edges
-    print("Time take to compute graph " + str((t.datetime.now() - start_time).seconds) + " sec")
     return graph
 
 
 def bfs(graph, src, dest):
+    start_time = t.datetime.now()
     visited = []  # keep track of visited nodes
     queue = [src]  # queue for implementing BFS; add src node to the queue
     path = {}
@@ -65,17 +62,60 @@ def bfs(graph, src, dest):
         # Remove one node from the queue and check if it has been visited or not
         node = queue.pop(0)
         if node == dest:
-            break
+            path = get_path(path, src, dest)
+            timetaken = (t.datetime.now() - start_time).microseconds
+            return ["S", node, path, timetaken]
         # get neighbors
-        neighbors = graph[node]
-        print(node, "->", neighbors)
+        neighbors = graph.get(node)
+        # print(node, "->", neighbors)
         for neighbor in neighbors:
-            if neighbor not in visited:
+            if neighbor not in visited and neighbor not in queue:
                 # visit neighbors and add to queue
                 queue.append(neighbor)
                 path[neighbor] = node
         visited.append(node)
-    return path
+    timetaken = (t.datetime.now() - start_time).microseconds
+    return ["F", None, None, timetaken]
+
+
+def dfs(graph, src, dest):
+    start_time = t.datetime.now()
+    visited = []  # keep track of visited nodes
+    stack = [src]  # queue for implementing BFS; add src node to the queue
+    path = {}
+    if src == dest:
+        return "Source = Destination. Maze is solved."
+    # Run until the queue is empty
+    while stack:
+        # Remove one node from the queue and check if it has been visited or not
+        node = stack.pop()
+        if node == dest:
+            path = get_path(path, src, dest)
+            timetakem = (t.datetime.now() - start_time).microseconds
+            return ["S", node, path, timetakem]
+        # get neighbors
+        neighbors = graph[node]
+        print(node, "->", neighbors)
+        for neighbor in neighbors:
+            if neighbor not in visited and neighbor not in stack:
+                # visit neighbors and add to queue
+                stack.append(neighbor)
+                path[neighbor] = node
+        visited.append(node)
+    timetakem = (t.datetime.now() - start_time).microseconds
+    return ["F", None, None, timetakem]
+
+
+def get_path(path, src, dest):
+    pathtaken = [dest]
+    child = dest
+    parent = dest
+    while parent != src:
+        parent = path.get(child)
+        pathtaken.append(parent)
+        child = parent
+    pathtaken.reverse()
+    return pathtaken
 
 
 def get_neighbour(maze, step, i, j):
@@ -134,7 +174,7 @@ def display(items):
     fig, ax = plt.subplots(1, (len(items)))
     for index, item in enumerate(items):
         size = item.shape[0]
-        ax[index].matshow(item, cmap=cm.binary, aspect='equal')
+        ax[index].matshow(item, cmap=cm.binary,)
         ax[index].set_xticks(np.arange(-0.5, size, 1))
         ax[index].set_yticks(np.arange(-0.5, size, 1))
         ax[index].set_xticklabels(np.arange(0, size + 1, 1), rotation=90, horizontalalignment="right")
@@ -143,7 +183,41 @@ def display(items):
     plt.show()
 
 
-sols = create_mazes(10, 30, 0.1)
-sol = create_graph(sols[0])
-print(sol)
-display(sols)
+def letsfind():
+    startsize = int(input("Enter the starting  size "))
+    endsize = int(input("Enter the max size "))
+    step = int(input("Enter the growth size "))
+    blockdensity = float(input("Enter the block density parameter(from 0 to 1) "))
+    data = {}
+    for index, size in enumerate(range(startsize, endsize + step, step)):
+        maze = create_maze(size, blockdensity)
+        print("Maze Moving to Next")
+        graph = create_graph(maze)
+        print("Graph Moving to Next"+ str(graph))
+        start = (0, 0)
+        end = ((maze.shape[0] - 1), (maze.shape[0] - 1))
+        bfs_sol = bfs(graph, start, end)
+        print("BFS  Moving to Next")
+        dfs_sol = dfs(graph, start, end)
+        print("DFS  Moving to Next")
+        data[index] = {"size": size, "maze": maze, "bfs_path": bfs_sol[2], "bfs_time": bfs_sol[3],
+                       "dfs_path": dfs_sol[2], "dfs_time": dfs_sol[3]}
+        print("Moving to Next Maze")
+
+    maze_size = list(map(lambda key: (data.get(key)).get("size"), data.keys()))
+    mazes = list(map(lambda key: (data.get(key)).get("maze"), data.keys()))
+    bfs_time = list(map(lambda key: (data.get(key)).get("bfs_time"), data.keys()))
+    bfs_path = list(map(lambda key: (data.get(key)).get("bfs_path"), data.keys()))
+    dfs_time = list(map(lambda key: (data.get(key)).get("dfs_time"), data.keys()))
+    dfs_path = list(map(lambda key: (data.get(key)).get("dfs_path"), data.keys()))
+
+    print(bfs_path)
+    print(dfs_path)
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].scatter(maze_size, bfs_time)
+    ax[1].scatter(maze_size, dfs_time)
+    display(mazes)
+
+
+letsfind()
