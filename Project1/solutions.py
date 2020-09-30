b71ae638b8a54aff3c01fa29653a8f8749a59eb4
Pranav Shivkumar <pranavshivkumar3@gmail.com>
2020-09-30 19:23:28 -0400

    tried something for q rate

--- Project1/solutions.py
+++ Project1/solutions.py
@@ -66,33 +66,33 @@ def display(m, si):
 # Agent follows the searched path without changing or recomputing it.
 # 'f1' is the starting point of fire
 # Using Bidirectional BFS to find the shortest path from Algorithm class
-def sol1(maze, size, graph1, src, dest, f1):
-    print(graph1)
-    result1 = al.bibfs(graph1, src, dest)
+def sol1(maze1, size1, graph1, src1, dest1, f1, q):
+    # print(graph1)
+    result1 = al.bibfs(graph1, src1, dest1)
     print(result1)
-    maze[0][0] = 2
-    maze[size - 1][size - 1] = 5
+    maze1[0][0] = 2
+    maze1[size1 - 1][size1 - 1] = 5
     nodes_on_fire = []
-    if result1[0] == "S" and fn is not None:
-        maze[fn[0]][fn[1]] = 3
-        nodes_on_fire.append(f1)
-        print(result1[2][0])
-        result1[2].pop(0)
-        for i in range(len(result1[2])):
-            step = result1[2].pop(0)
-            print(step)
-            maze[step[0]][step[1]] = 2
-            spread_fire(graph1, nodes_on_fire, 0.2, src, dest)
-            print("nodes on fire --->" + str(nodes_on_fire))
-            if step in nodes_on_fire:
-                print("Death by fire")
-                maze[step[0]][step[1]] = 4
-                nodes_on_fire.remove(step)
-                break
-        for i in nodes_on_fire:
-            maze[i[0]][i[1]] = 3
-        display(maze, size)
-
+    # if result1[0] == "S" and fn is not None:
+    maze1[fn[0]][fn[1]] = 3
+    nodes_on_fire.append(f1)
+    # print(result1[2][0])
+    result1[2].pop(0)
+    for i in range(len(result1[2])):
+        step1 = result1[2].pop(0)
+        # print(step1)
+        maze1[step1[0]][step1[1]] = 2
+        spread_fire(graph1, nodes_on_fire, q, src1, dest1)
+        # print("nodes on fire --->" + str(nodes_on_fire))
+        if step1 in nodes_on_fire:
+            # print("Death by fire")
+            maze1[step1[0]][step1[1]] = 4
+            nodes_on_fire.remove(step1)
+            return False
+    for i in nodes_on_fire:
+        maze1[i[0]][i[1]] = 3
+    display(maze1, size1)
+    return True
 
 # Needs more explanation
 # Solution 2
@@ -100,41 +100,39 @@ def sol1(maze, size, graph1, src, dest, f1):
 # Recomputing takes places when any of the given path node is on fire
 # 'f1' is the starting point of fire
 # Using Bidirectional BFS to find the shortest path from Algorithm class
-def sol2(maze1, size, graph1, src, dest, f1):
-    result2 = al.bibfs(graph1, src, dest)
-    print(result2)
-    maze1[0][0] = 2
-    maze1[size - 1][size - 1] = 5
+def sol2(maze2, size2, graph2, src2, dest2, f2,q):
+    result2 = al.bibfs(graph2, src2, dest2)
+    # print(result2)
+    maze2[0][0] = 2
+    maze2[size2 - 1][size2 - 1] = 5
     nodes_on_fire = []
-    if result2[0] == "S" and fn is not None:
-        maze1[fn[0]][fn[1]] = 3
-        nodes_on_fire.append(f1)
-        dFlag = "A"
-        step = src
-        while True:
-            result2 = al.bibfs(mz.create_graph(maze1), step, dest)
-            if result2[2] is None:
-                print("Death by trap")
-                break
-            print(result2[2])
-            step = result2[2].pop(1)
-            print(step)
-            maze1[step[0]][step[1]] = 2
-            if step == dest:
-                print("Success")
-                break
-            spread_fire(graph1, nodes_on_fire, 0.2, src, dest)
-            for i in nodes_on_fire:
-                maze1[i[0]][i[1]] = 3
-            print("nodes on fire --->" + str(nodes_on_fire))
-            if step in nodes_on_fire:
-                print("Death by fire")
-                dFlag = "D"
-                maze1[step[0]][step[1]] = 4
-                nodes_on_fire.remove(step)
-                break
+    # if result2[0] == "S" and fn is not None:
+    maze2[fn[0]][fn[1]] = 3
+    nodes_on_fire.append(f2)
+    step = src2
+    while True:
+        result2 = al.bibfs(mz.create_graph(maze2), step, dest2)
+        if not result2[2]:
+            # print("Death by trap")
+            return False
+        # print(result2[2])
+        step = result2[2].pop(1)
+        # print(step)
+        maze2[step[0]][step[1]] = 2
+        if step == dest2:
+            # print("Success")
+            return True
+        spread_fire(graph2, nodes_on_fire, q, src2, dest2)
+        for i in nodes_on_fire:
+            maze2[i[0]][i[1]] = 3
+        # print("nodes on fire --->" + str(nodes_on_fire))
+        if step in nodes_on_fire:
+            # print("Death by fire")
+            maze2[step[0]][step[1]] = 4
+            nodes_on_fire.remove(step)
+            return False
 
-    display(maze1, size)
+    display(maze2, size2)
 
 
 # Needs more explanation
@@ -144,14 +142,14 @@ def sol2(maze1, size, graph1, src, dest, f1):
 # different path
 # 'f1' is the starting point of fire
 # Using Bidirectional BFS to find the shortest path from Algorithm class
-def sol3(maze1, size, graph1, src, dest, f1):
+def sol3(maze3, size3, graph3, src3, dest3, f3, q):
 
     def feelthefire(gr, st, fire, level):  # gr =  graph, src = source, fire = nodes on fire, level = depth
         currentnode = st
-        print(level)
-        print(currentnode)
+        # print(level)
+        # print(currentnode)
         if currentnode in fire:
-            print(currentnode)
+            # print(currentnode)
             return True
 
         # If reached the maximum depth, stop recursing.
@@ -162,50 +160,52 @@ def sol3(maze1, size, graph1, src, dest, f1):
                 for neigh in gr[st]:
                     if neigh not in ff:
                         ff.add(currentnode)
-                        if feelthefire(gr, i, fire, level - 1):
+                        if feelthefire(gr, neigh, fire, level - 1):
                             return True
         return False
 
-    result3 = al.bibfs(graph1, src, dest)
+    result3 = al.bibfs(graph3, src3, dest3)
     print(result3)
-    maze1[0][0] = 2                             # explaination
-    maze1[size - 1][size - 1] = 5
+    maze3[0][0] = 2                             # explaination
+    maze3[size3 - 1][size3 - 1] = 5
     nodes_on_fire = []
     if result3[0] == "S" and fn is not None:
-        maze1[fn[0]][fn[1]] = 3
-        nodes_on_fire.append(f1)
-        prevnode = src
-        result3[2].pop(0)
+        maze3[fn[0]][fn[1]] = 3
+        nodes_on_fire.append(f3)
+        prevnode = src3
+        #result3[2].pop(0)
         while True:
-            step = result3[2].pop(0)
+            print(prevnode)
+            step = result3[2].pop(1)
             print("Suggest step ->" + str(step))
+            if step == dest3:
+                print("Success")
+                return True
             ff = set([])
-            check = feelthefire(graph1, prevnode, nodes_on_fire, 3)
+            check = feelthefire(graph3, prevnode, nodes_on_fire, 3)
             print(check)
             if check:
-                result3 = al.bibfs(mz.create_graph(maze1), prevnode, dest)
+                result3 = al.bibfs(mz.create_graph(maze3), prevnode, dest3)
                 print("changed path")
-                if result3[2] is None:
+                print(result3)
+                if not result3[2]:
                     print("Death by trap")
-                    break
+                    return False
                 print(result3[2])
                 step = result3[2].pop(1)
             prevnode = step
-            print("Step Taken---->" + str(step))
-            maze1[step[0]][step[1]] = 2
-            if step == dest:
-                print("Success")
-                break
-            spread_fire(graph1, nodes_on_fire, 0.2, src, dest)
+            # print("Step Taken---->" + str(step))
+            maze3[step[0]][step[1]] = 2
+            spread_fire(graph3, nodes_on_fire, q, src3, dest3)
             for i in nodes_on_fire:
-                maze1[i[0]][i[1]] = 3
-            print("nodes on fire --->" + str(nodes_on_fire))
+                maze3[i[0]][i[1]] = 3
+            # print("nodes on fire --->" + str(nodes_on_fire))
             if step in nodes_on_fire:
                 print("Death by fire")
-                maze1[step[0]][step[1]] = 4
+                maze3[step[0]][step[1]] = 4
                 nodes_on_fire.remove(step)
-                break
-    display(maze1, size)
+                return False
+    display(maze3, size3)
 
 
 s = 5                                    # MaxDepth
@@ -213,20 +213,58 @@ sr = (0, 0)                              # Starting node
 des = (s - 1, s - 1)                     # Destination
 m1 = mz.create_maze(s, 0.2)              # Create maze function
 gr1 = mz.create_graph(m1)                # Then create graph
-m2 = m1.copy()               # maze
-gr2 = gr1.copy()               # graph
+# m2 = m1.copy()               # maze
+# gr2 = gr1.copy()               # graph
 m3 = m1.copy()               # maze
 gr3 = gr1.copy()               # graph
 fn = let_there_be_fire(gr1, sr, des)              # initializes fire
-print("Fire Starts at" + str(fn))
-print("User Starts at" + str(sr))
-# Solution 1
-print("SOL1")
-sol1(m1, s, gr1, sr, des, fn)            # m1 and gr1 used
-# Solution 2
-print("SOL2")
-sol2(m2, s, gr2, sr, des, fn)            # m2 and gr2 used
-# Solution 1
+# print("Fire Starts at" + str(fn))
+# print("User Starts at" + str(sr))
+# # Solution 1
+# print("SOL1")
+# sol1(m1, s, gr1, sr, des, fn)            # m1 and gr1 used
+# # Solution 2
+# print("SOL2")
+# sol2(m2, s, gr2, sr, des, fn)            # m2 and gr2 used
+# # Solution 1
 print("SOL3")
-sol3(m3, s, gr3, sr, des, fn)            # m3 and gr3 used
+sol3(m3, s, gr3, sr, des, fn, 0.1)            # m3 and gr3 used
 plt.show()
+
+# q1 = [0.05, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.5]
+# result = {}
+# for q in q1:
+#     m1 = mz.create_maze(s, 0.3)  # Create maze function
+#     gr1 = mz.create_graph(m1)  # Then create graph
+#     m2 = m1.copy()  # maze
+#     gr2 = gr1.copy()  # graph
+#     m3 = m1.copy()  # maze
+#     gr3 = gr1.copy()  # graph
+#     fn = let_there_be_fire(gr1, sr, des)  # initializes fire
+#     print("Fire Starts at" + str(fn))
+#     print("User Starts at" + str(sr))
+#
+#     count1, count2, count3 = 0, 0, 0
+#     counter = 0
+#     if al.bibfs(gr1, sr, des)[0] == 'S' and fn is not None:
+#         while counter < 10:
+#             # Solution 1
+#             f1 = sol1(m1, s, gr1, sr, des, fn, q)  # m1 and gr1 used
+#             if f1:
+#                 count1 += 1
+#             counter += 1
+#             # Solution 2
+#             print("SOL2")
+#             f2 = sol2(m2, s, gr2, sr, des, fn, q)  # m2 and gr2 used
+#             if f2:
+#                 count2 += 1
+#             counter += 1
+#             # Solution 3
+#             print("SOL3")
+#             f3 = sol3(m3, s, gr3, sr, des, fn, q)  # m3 and gr3 used
+#             if f3:
+#                 count3 += 1
+#             counter += 1
+#
+#     result[q] = [count1, count2, count3]
+# print(result)
