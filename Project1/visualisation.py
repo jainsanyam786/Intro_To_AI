d3d8ab0b657fc9412ee39e72fef8f29d2a98c832
kaustubh jadhav <knj25@scarletmail.rutgers.edu>
2020-10-03 21:09:11 -0400

    Commented

--- Project1/visualisation.py
+++ Project1/visualisation.py
@@ -9,11 +9,12 @@ import matplotlib.gridspec as gridspec
 # This function helps to display the maze.
 # matplotlib library is imported and to generate a figure.
 # Configured the maze with required specifications
+# This is called in analysis.py and maze_thinning.py
 def display_maze(item, size, title):
-    fig = plt.figure()
+    fig = plt.figure()                                       # Initializing a figure
     ax = fig.add_subplot()
     ax.matshow(item, cmap=cm.binary)
-    ax.set_title(title)
+    ax.set_title(title)                                      # Adding title
     ax.set_xticks(np.arange(-0.5, size, 1))
     ax.set_yticks(np.arange(-0.5, size, 1))
     ax.set_xticklabels(np.arange(0, size + 1, 1), rotation=90, horizontalalignment="right")
@@ -22,18 +23,19 @@ def display_maze(item, size, title):
 
 
 # Size vs Path Length  specific for probability of 0.3
+# This is called in analysis.py
 def disp_path_for_probab3(data, startsize, endsize, step):
-    fig = plt.figure()
+    fig = plt.figure()                                          # Initializing figure
     ax1 = fig.add_subplot()
     ax1.set_xlabel("Size")
     ax1.set_ylabel("Path")
     ax1.set_title("Size vs Path Length with probability 0.3")
-    size_list = []
+    size_list = []                                              # List to append the sizes of maze
     for size in range(startsize, endsize + step, step):
         size_list.append(size)
-    algo_list = ["bfs", "dfs", "dijk", "bibfs"]
+    algo_list = ["bfs", "dfs", "dijk", "bibfs"]                 # List of search algorithms
     for algo in algo_list:
-        d = data.get(0.3)
+        d = data.get(0.3)                                       # Fixed probability of 0.3 and getting graphs
         path = list(map(lambda key: (d.get(key)).get(algo + "_path"), d.keys()))
         ax1.scatter(size_list, path, label=algo)
     ax1.legend(title="Search Algorithms")
@@ -41,18 +43,19 @@ def disp_path_for_probab3(data, startsize, endsize, step):
 
 
 # Size vs Time  specific for probability of 0.3
+# This is called in analysis.py
 def disp_time_for_probab3(data, startsize, endsize, step):
-    fig = plt.figure()
+    fig = plt.figure()                                      # Initializing figure
     ax1 = fig.add_subplot()
     ax1.set_xlabel("Size")
     ax1.set_ylabel("Time (micro sec)")
     ax1.set_title("Size vs Time with probability 0.3")
-    size_list = []
+    size_list = []                                          # List to append the sizes of maze
     for size in range(startsize, endsize + step, step):
         size_list.append(size)
     algo_list = ["bfs", "dfs", "dijk", "bibfs"]
     for algo in algo_list:
-        d = data.get(0.3)
+        d = data.get(0.3)                                   # Fixed probability of 0.3 and getting graphs
         time = list(map(lambda key: (d.get(key)).get(algo + "_time"), d.keys()))
         ax1.scatter(size_list, time, label=algo)
     ax1.legend(title="Search Algorithms")
@@ -61,19 +64,19 @@ def disp_time_for_probab3(data, startsize, endsize, step):
 
 # Size vs Success Rate
 # Probability are 0.1, 0.3, 0.4, 0.5, 0.7 and 0.9
+# This is called in analysis.py
 def disp_stats_for_probab(data, startsize, endsize, step, probability_list):
-    fig = plt.figure()
+    fig = plt.figure()                                             # Initializing figure
     ax1 = fig.add_subplot()
     ax1.set_xlabel("Size")
     ax1.set_ylabel("Success Rate out of 10")
     ax1.set_title("Success Rate vs Size ")
-    size_list = []
-
+    size_list = []                                          # List to append the sizes of maze
     for size in range(startsize, endsize + step, step):
         size_list.append(size)
 
     for prob in probability_list:
-        d = data.get(prob)
+        d = data.get(prob)                                    # Extract information with index as probability
         success = list(map(lambda key: (d.get(key)).get("TotalSuccessRate"), d.keys()))
         ax1.scatter(size_list, success, label=prob)
 
@@ -85,6 +88,7 @@ def disp_stats_for_probab(data, startsize, endsize, step, probability_list):
 # Function used to display the maze after the agent is burned or reached destination
 # m is maze and si is size
 # 'bounds' is used as enumerator for each color node
+# This is called in solutions.py
 def display_maze_onfire(m, si, flamability, solut):
     cmap = ListedColormap(['w', 'k', 'c', 'y', 'r', 'g'])
     bounds = [0, 1, 2, 3, 4, 5, 6]
@@ -103,6 +107,7 @@ def display_maze_onfire(m, si, flamability, solut):
 
 
 # Flamability vs variable(Time/Success rate)
+# This is called in solutions.py
 def disp_graph_maze_onfire(data, flamabilityList, xlabel, ylabel, title, varlist):
     fig = plt.figure()
     ax1 = fig.add_subplot()
@@ -117,6 +122,7 @@ def disp_graph_maze_onfire(data, flamabilityList, xlabel, ylabel, title, varlist
     ax1.grid(True)
 
 
+# This is called in maze_thinning.py
 def dispdata(data, name, sizelist, title):
     fig = plt.figure()
     ax1 = fig.add_subplot()
@@ -129,8 +135,9 @@ def dispdata(data, name, sizelist, title):
     ax1.grid(True)
 
 
+# This is called in maze_thinning.py
 def disp_data(data, varname, xlable, ylabel, title):
-    fig = plt.figure()
+    fig = plt.figure()                               # Initializing figure
     ax1 = fig.add_subplot()
     ax1.set_xlabel(xlable)
     ax1.set_ylabel(ylabel)
@@ -147,6 +154,7 @@ def disp_data(data, varname, xlable, ylabel, title):
     ax1.grid(True)
 
 
+# This is called in maze_thinning.py
 def disp_data2(data, varname, ylabel, xlable, title):
     fig = plt.figure()
     ax1 = fig.add_subplot()
