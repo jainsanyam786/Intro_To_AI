3e43144e4003d72dd69101b9094dc9a91224b78f
kaustubh jadhav <knj25@scarletmail.rutgers.edu>
2020-11-12 17:53:16 -0500

    Optimized and commented

--- Project3/ProbabSearchinteractive.py
+++ Project3/ProbabSearchinteractive.py
@@ -16,9 +16,14 @@ class ProbabilisticHunting:
         self.cells = set([])
         self.diffProbDict = diffProbDict
 
+    # Creating a target at a random x and y location
     def gettarget(self):
         return randint(0, self.size - 1), randint(0, self.size - 1)
 
+    # Landscape creation
+    # num holds a square matrix, rows and columns equal to total size provided by user
+    # Later this matrix is converted into array and thus a landscape without GUI is created.
+    # After this a target is randomly selected and printed on console
     def create_landscape(self):
         num = [[np.random.choice(np.arange(4), 1, p=self.probabilities)[0] for i in range(self.size)] for j in
                range(self.size)]
@@ -28,6 +33,8 @@ class ProbabilisticHunting:
         self.target = self.gettarget()
         print(num_arr[self.target[0]][self.target[1]])
 
+    # Landscape Display GUI based
+    # Coloring based on the probabilities assigned to each block
     def display_landscape(self):
         cmap = ListedColormap(['#FFFFFF', '#A0A0A0', '#009900', '#404040'])
         bounds = [0, 1, 2, 3, 4]
@@ -44,10 +51,14 @@ class ProbabilisticHunting:
         ax.text(self.target[1], self.target[0], "X", ha="center", va="center", color="#990000", fontsize=20)
         ax.grid(color='k', linestyle='-', linewidth=2)
 
+    # Updating initial probabilities database
     def probabilitydictionary(self):
         [[self.targetLocprobabdict.update({(x, y): (1 / self.size ** 2)}) for y in range(self.size)]
          for x in range(self.size)]
 
+    # Updating each instance of probability database as per object
+    # This is based on Part 1, both the equations of the part 1 are implemented below to update the probabilities
+    # required for all the agents
     def updateprobabilities(self, tosearch, observation):
         for cell in self.targetLocprobabdict.keys():
             if cell is tosearch:
@@ -57,6 +68,8 @@ class ProbabilisticHunting:
                 prob = self.targetLocprobabdict.get(cell) / observation
             self.targetLocprobabdict.update({cell: prob})
 
+    # Used for Agent 2
+    # Probability dictionary of finding a target is created and updated
     def gettargetfoundprobabilities(self):
         targefoundprobabdict = {}
         for cell in self.targetLocprobabdict.keys():
@@ -65,6 +78,7 @@ class ProbabilisticHunting:
             targefoundprobabdict[cell] = foundprob
         return targefoundprobabdict
 
+    # Function used to get the total score, i.e total steps taken by each agent
     def getcellscores(self,currentlocation):
         targefoundprobabdict = {}
         cellscores = {}
@@ -170,22 +184,24 @@ class ProbabilisticHunting:
             currentlocation = tosearch
 
 
+# All the agents are included
+# actions means how many steps did it take to find the target
 def main():
     input1 = int(input("Enter the size: "))
-    prob = [0.2, 0.3, 0.3, 0.2]
-    diffProbDict = {0: 0.1, 1: 0.3, 2: 0.7, 3: 0.9}
-    landscape = ProbabilisticHunting(input1, prob, diffProbDict)
-    landscape.create_landscape()
+    prob = [0.2, 0.3, 0.3, 0.2]               # The map is divided with theses probabilities, equalling total to 1
+    diffProbDict = {0: 0.1, 1: 0.3, 2: 0.7, 3: 0.9}             # Probabilities under each closed block at random
+    landscape = ProbabilisticHunting(input1, prob, diffProbDict)    # object creation and assigning values
+    landscape.create_landscape()                                    # Creating landscape
     # landscape.display_landscape()
     landscape.probabilitydictionary()
     print()
-    print("target cell and actions" + str(landscape.gamerule1()))
+    print("target cell and actions" + str(landscape.gamerule1()))               # Agent 1
     landscape.probabilitydictionary()
     print()
-    print("target cell and actions" + str(landscape.gamerule2()))
+    print("target cell and actions" + str(landscape.gamerule2()))               # Agent 2
     landscape.probabilitydictionary()
     print()
-    print("target cell and actions" + str(landscape.gamerule3()))
+    print("target cell and actions" + str(landscape.gamerule3()))               # Agent 3
     plt.show()
 
 
